1480fdf6c839dfa26e253266e7ca8600744e0c86
Rafael <88090582+rafa-san@users.noreply.github.com>
2021-12-28 17:20:42 -0300

    Testes no exemplo 3

--- scripts/exemplo3-efeito-scroll.js
+++ scripts/exemplo3-efeito-scroll.js
@@ -1,9 +1,18 @@
 var nav = document.getElementById('efeito');
+var a1900 = document.querySelector('.a1900');
+var a1930 = document.querySelector('.a1930');
 
 document.body.onscroll = () => {
-  if (window.pageYOffset < 400) {
-    nav.style.display = "none";
+
+  if (window.pageYOffset < 66) {
+      nav.style.display = "none";
+  } else if (window.pageYOffset >= 66 && window.pageYOffset < 464) {
+      nav.style.display = "block";
+      a1900.style.backgroundColor = "orange";
+  } else if (window.pageYOffset > 464 && window.pageYOffset < 862) {
+      nav.style.display = "block";
+      a1930.style.backgroundColor = "yellow";
   } else {
-    nav.style.display = "block";
+      nav.style.display = "block";
   }
 }
