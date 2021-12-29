50025d15b44899cf9a8920fb038f21ff2f4fbb41
Rafael <88090582+rafa-san@users.noreply.github.com>
2021-12-28 23:12:13 -0300

    Atualizações no exemplo 3
    Resolvi manter a linha do tempo da forma mais simples possível.

--- scripts/exemplo3-efeito-scroll.js
+++ scripts/exemplo3-efeito-scroll.js
@@ -1,18 +1,10 @@
-var nav = document.getElementById('efeito');
-var a1900 = document.querySelector('.a1900');
-var a1930 = document.querySelector('.a1930');
+var anos = document.querySelector('.conteudo-dropdown');
 
 document.body.onscroll = () => {
 
-  if (window.pageYOffset < 66) {
-      nav.style.display = "none";
-  } else if (window.pageYOffset >= 66 && window.pageYOffset < 464) {
-      nav.style.display = "block";
-      a1900.style.backgroundColor = "orange";
-  } else if (window.pageYOffset > 464 && window.pageYOffset < 862) {
-      nav.style.display = "block";
-      a1930.style.backgroundColor = "yellow";
+  if (window.pageYOffset < 420) {
+      anos.style.display = "none";
   } else {
-      nav.style.display = "block";
+      anos.style.display = "block";
   }
 }
